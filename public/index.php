87bfa60834a29dbb4c29d5a8ba301cc74300b77b
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-10-22 11:38:05 +0200

    homepage définie sur le choix du perso

--- public/index.php
+++ public/index.php
@@ -1,20 +1,24 @@
+
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/view');
 $twig = new Twig\Environment($loader);
 
+$rooting = explode('/',$_SERVER['REQUEST_URI']);
 
-
-$rooting = explode("/",$_SERVER['REQUEST_URI']);
-
-$controler = 'App\Controller\\' . $rooting[1];
+if ($rooting === ['','']) {
+    $controler ='App\Controller\Fight';
+    $model = 'select';
+    $parameter = null;
+} else {
+    $controler = 'App\Controller\\' . $rooting[1];
 $model = $rooting[2];
-$parameter = isset($rooting[3]) ? $rooting[3]: null;
+$parameter = isset($rooting[3]) ? $rooting[3]: null; }
 
 $twigChoice = new $controler;
 $test = $twigChoice->$model($parameter);
-
 
 echo $twig->render($test['view'], $test['parameter']);
 
